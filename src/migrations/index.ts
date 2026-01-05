0adc0970f3ce56daf3176df085ffdfbda71e0528
blnayan <52083057+blnayan@users.noreply.github.com>
2026-01-05 14:42:52 -0500

    feat: Add new database migration for new collection inquiries.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260101_221310_initial from './20260101_221310_initial';
+import * as migration_20260105_194231_new_collection_inquiries from './20260105_194231_new_collection_inquiries';
 
 export const migrations = [
   {
     up: migration_20260101_221310_initial.up,
     down: migration_20260101_221310_initial.down,
-    name: '20260101_221310_initial'
+    name: '20260101_221310_initial',
+  },
+  {
+    up: migration_20260105_194231_new_collection_inquiries.up,
+    down: migration_20260105_194231_new_collection_inquiries.down,
+    name: '20260105_194231_new_collection_inquiries'
   },
 ];
