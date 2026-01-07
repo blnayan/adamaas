c2d817784838b8a660b294b66649b386d464294b
blnayan <52083057+blnayan@users.noreply.github.com>
2026-01-06 21:26:08 -0500

    feat: Update product afterChange hook to conditionally revalidate a specific shop page based on product type and creation date.

--- src/payload/collections/Products.ts
+++ src/payload/collections/Products.ts
@@ -1,113 +1,136 @@
-import { Product } from '@/payload-types'
-import type { CollectionConfig, CollectionAfterChangeHook } from 'payload'
-import { revalidatePath } from 'next/cache'
+import { Product } from "@/payload-types";
+import {
+  type CollectionConfig,
+  type CollectionAfterChangeHook,
+  getPayload,
+} from "payload";
+import config from "@payload-config";
+import { revalidatePath } from "next/cache";
 
 export const Products: CollectionConfig = {
-  slug: 'products',
+  slug: "products",
   admin: {
-    useAsTitle: 'name',
+    useAsTitle: "name",
   },
   access: {
     read: () => true,
   },
   hooks: {
     afterChange: [
-      async ({data}) => {
-        revalidatePath('/')
-        revalidatePath(`/product/${data.slug}`)
-        revalidatePath(`/shop/[page]`)
+      async ({ data, context }) => {
+        const payload = await getPayload({ config });
+
+        revalidatePath("/");
+        revalidatePath(`/product/${data.slug}`);
+
+        if (data.type === "product") {
+          const limit = 9;
+          const { totalDocs } = await payload.count({
+            collection: "products",
+            where: {
+              type: {
+                equals: "product",
+              },
+              createdAt: {
+                greater_than: data.createdAt,
+              },
+            },
+          });
+          const page = Math.floor(totalDocs / limit) + 1;
+          revalidatePath(`/shop/${page}`);
+        }
       },
     ] as CollectionAfterChangeHook<Product>[],
   },
   fields: [
     {
-      name: 'name',
-      type: 'text',
+      name: "name",
+      type: "text",
       required: true,
     },
     {
-      name: 'type',
-      type: 'select',
+      name: "type",
+      type: "select",
       options: [
-        { label: 'Product', value: 'product' },
-        { label: 'Bundle', value: 'bundle' },
+        { label: "Product", value: "product" },
+        { label: "Bundle", value: "bundle" },
       ],
-      defaultValue: 'product',
+      defaultValue: "product",
       required: true,
     },
     {
-      name: 'slug',
-      type: 'text',
+      name: "slug",
+      type: "text",
       required: true,
       unique: true, // Slugs must be unique
     },
     {
-      name: 'tagline',
-      type: 'text',
+      name: "tagline",
+      type: "text",
       required: true,
     },
     {
-      name: 'basePrice',
-      type: 'number',
+      name: "basePrice",
+      type: "number",
       required: true,
       min: 0,
     },
     {
-      name: 'description',
-      type: 'textarea',
+      name: "description",
+      type: "textarea",
       required: true,
     },
     {
-      name: 'badges',
-      type: 'array',
-      label: 'Badges',
+      name: "badges",
+      type: "array",
+      label: "Badges",
       minRows: 0,
       fields: [
         {
-          name: 'text',
-          type: 'text',
+          name: "text",
+          type: "text",
         },
       ],
     },
     {
-      name: 'variants',
-      type: 'array',
-      label: 'Variants',
+      name: "variants",
+      type: "array",
+      label: "Variants",
       fields: [
         {
-          name: 'name',
-          type: 'text',
+          name: "name",
+          type: "text",
           required: true,
         },
         {
-          name: 'price',
-          type: 'number',
+          name: "price",
+          type: "number",
           required: true,
           min: 0,
         },
       ],
     },
     {
-      name: 'techSpecs',
-      type: 'array',
-      label: 'Technical Specifications',
+      name: "techSpecs",
+      type: "array",
+      label: "Technical Specifications",
       fields: [
         {
-          name: 'label',
-          type: 'text',
+          name: "label",
+          type: "text",
           required: true,
         },
         {
-          name: 'value',
-          type: 'text',
+          name: "value",
+          type: "text",
           required: true,
         },
       ],
     },
     {
-      name: 'image',
-      type: 'upload',
-      relationTo: 'media',
+      name: "image",
+      type: "upload",
+      relationTo: "media",
     },
   ],
-}
+};
